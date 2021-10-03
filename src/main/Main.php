11e25df579312e3fb346f754f3a4a8ad7cff53b7
Cubecraft0 <90528229+Cubecraft0@users.noreply.github.com>
2021-10-03 13:21:19 +0200

    Php

--- src/main/Main.php
+++ src/main/Main.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace main;
+namespace src\main;
 
-use .\pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
   public function onEnable(){
